84693e29bb24faea9cdf4ed39474bb3c7ca330e2
David Ladowitz <david@ladowitz.com>
2018-01-14 11:23:16 -0800

    formatting

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -2,9 +2,9 @@ class HelpController < ApplicationController
   def index
 
     render plain: " `/splack` requires three variables @userName, amount, coin separated by spaces
-      /set_address sets your receive address
-      /view_address views the address you have set to recieve
-      /check_balance checks your current Coinbase balances
-      /hello says hello"
+      `/set_address` sets your receive address
+      `/view_address` views the address you have set to recieve
+      `/check_balance` checks your current Coinbase balances
+      `/hello` says hello"
   end
 end
