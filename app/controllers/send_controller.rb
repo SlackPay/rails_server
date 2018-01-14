325f8302eb9be5d610e99c9561e762ba2a593c81
David Ladowitz <david@ladowitz.com>
2018-01-13 19:05:58 -0800

    Create response

--- app/controllers/send_controller.rb
+++ app/controllers/send_controller.rb
@@ -8,7 +8,7 @@ class SendController < ApplicationController
 
     respond_to do |format|
       # format.html
-      format.json { render json: {amount: @amount} }
+      format.json { render json: generate_response }
     end
   end
 
@@ -17,4 +17,34 @@ class SendController < ApplicationController
     true # get a api key from request
   end
 
+  def generate_response
+    {
+       "ok": true,
+       "channel": params[:channel_id],
+       "ts": Time.now.to_i,
+       "message": {
+           "text": "Here’s a message for you",
+           "username": params[:user_name],
+           "bot_id": "????????",
+           "attachments": [
+               {
+                   "text": "This is an attachment",
+                   "id": 1,
+                   "fallback": "This is an attachment’s fallback"
+               }
+           ],
+           "type": "message",
+           "subtype": "bot_message",
+           "ts": Time.now.to_i
+       }
+    }
+
+  end
 end
+
+
+
+# Parameters: {"token"=>"WI7y8qD2fs4sD4QmVbUYuDlW", "team_id"=>"T8RRT17FC", "team_domain"=>"helios-coop",
+#               "channel_id"=>"C8RSSQMCY", "channel_name"=>"oasis", "user_id"=>"U8SNDPAS2", "user_name"=>"connor.bennette",
+#               "command"=>"/send", "text"=>".001 bch", "response_url"=>"https://hooks.slack.com/commands/T8RRT17FC/298492048899/jVF4ydCuaSbYsEJ8NXitmkR4",
+#               "trigger_id"=>"299651289671.297877041522.a389bd9a7aec0ae3fbbd3dab79cf4888"}
