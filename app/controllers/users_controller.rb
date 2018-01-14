51e829fdde4f811f2886350af5fc8325516ca39a
David Ladowitz <david@ladowitz.com>
2018-01-14 10:05:42 -0800

    Add view_address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,17 @@ class UsersController < ApplicationController
 
     render plain: "Success. Address set to #{receive_address}!"
   end
+
+  def view_address
+    slack_user_id   = params[:user_id]
+    slack_user_name = params[:user_name]
+
+    user = User.find_by(slack_user_id: slack_user_id)
+
+    if user
+      render plain: "You're current receive address is #{user.receive_address}"
+    else
+      render plain: "You haven't set a receive address yet. Use `/set_address xxxxxxxxxxx`."
+    end
+  end
 end
